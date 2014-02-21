a6c79060371818e4a69aafbea6e3e14869a44fdf
chris.candelora@americastestkitchen.com <chris.candelora@americastestkitchen.com>
2014-02-20 19:18:23 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bhsc::Application.config.secret_key_base = 'a187b5a2ba6bd8aa62beb4ad9e9d8d13a6ae6770b18249a3323e924b9259715a9a78c6828e73307d1bf033162305b25ecb0b931111dff79c85f60bfb3b9c1639'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bhsc::Application.config.session_store :cookie_store, key: '_bhsc_session'
